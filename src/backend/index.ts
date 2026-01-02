40635811111d80f8c99b130f6dfb615786e69c23
TanishqJangir <tanishqjangir001@gmail.com>
2026-01-02 18:28:58 +0530

    Added cors to the code

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -8,9 +8,12 @@ import { hash, compare } from "bcryptjs";
 import { z } from "zod";
 import { userMiddleware } from "../middleware/userMiddleware.js";
 import crypto from "crypto";
+import cors from "cors";
+
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 
 const JWT_USER_SECRET = process.env.JWT_USER_SECRET;
